191e6209907735ade947b724a6bea05ac109b1de
berypurda <berypurda@protonmail.com>
2022-03-27 18:26:25 +0200

    Firestore and Auth hooks complete

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import { useAuthContext } from "../hooks/useAuthContext"
+
+import { auth } from "../firebase/config"
+import { signInWithEmailAndPassword } from "firebase/auth"
+
+export const useLogin = () => {
+  const [error, setError] = useState(null)
+  const { dispatch } = useAuthContext()
+
+  const login = (email, password) => {
+    setError(null)
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        dispatch({ type: "LOGIN", payload: res })
+      })
+      .catch((err) => {
+        setError(err.message)
+      })
+  }
+
+  return { error, login }
+}
